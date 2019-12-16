169c2b8e1d63a96ba7a3e908741a837062d872aa
ravibrunsvik <ravi.brunsvik@gmail.com>
2019-12-16 19:28:59 +0100

    Fetch rewrite

--- modules/orders/js/orderFunctions.js
+++ modules/orders/js/orderFunctions.js
@@ -1,24 +1,36 @@
+let statusBar = document.getElementById("statusBar");
+
 function sendOrderConfirmation() {
-	
-if (document.getElementById('recipient').value=="") {
-	alert("obs: Ingen mottaker");
-	document.getElementById('recipient').focus()
-	exit;
-}
+	let recipient = document.getElementById("recipient");
 
-if (document.getElementById('recipientEmail').value=="") {
-	alert("obs: Ingen Epostadresse...");
-	document.getElementById('recipientEmail').focus()
-	exit;
-}
+	if (recipient.value === "") {
+		alert("obs: Ingen mottaker");
+		recipient.focus()
+		return;
+	}
+	let recipientEmail = document.getElementById("recipientEmail")
+	if (recipientEmail.value === "") {
+		alert("obs: Ingen Epostadresse...");
+		recipientEmail.focus()
+		return;
+	}
 
+	// what is this referencing?
 document.orderConfirmationForm.submit();
 	
 }
 
 function getOrder (orderID) {
+	// avoid inline styles, add a visible class to CSS.
 	document.getElementById("allButton").style.visibility='visible';
-		 
+
+	// rewrite to fetch
+	let list = document.getElementById("list");
+	fetch(`modules/orders/display_order.php?orderID=${orderID}`)
+	.then(res => list.innerHTML = res)
+		statusBar.innerHTML = "";
+
+
 xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function()  {
   if (xmlhttp.readyState==4 && xmlhttp.status==200) {
@@ -33,6 +45,8 @@ xmlhttp.open("GET","modules/orders/display_order.php?orderID="+orderID,true);
 xmlhttp.send();
 document.getElementById("statusBar").innerHTML="";
 
+
+
 }	
 
 
@@ -42,7 +56,19 @@ document.getElementById("unitPrice").value = price[priceID];
 
 
 function saveOrder() {
-	
+	let queryString = "?orderDate=" + encodeURIComponent(document.getElementById("orderDate").value) + 
+	"&unitPrice=" + encodeURIComponent(document.getElementById("unitPrice").value) +
+	"&orderComments=" + encodeURIComponent(document.getElementById("orderComments").value) +
+	"&creditDays=" + encodeURIComponent(document.getElementById("creditDays").value) +
+	"&otherTerms=" + encodeURIComponent(document.getElementById("otherTerms").value) +
+	"&customerContact=" + encodeURIComponent(document.getElementById("customerContact").value) +
+	"&productID=" + encodeURIComponent(document.getElementById("productID").value) +
+	"&regNumber=" + encodeURIComponent(document.getElementById("regNumber").value) +
+	"&orderID=" + encodeURIComponent(document.getElementById("orderID").value)  	
+	;
+	fetch(`modules/orders/process_order.php${queryString}`)
+	.then(res => statusBar.innerHTML = res)
+	// rewrite to fetch
 xmlhttpi=new XMLHttpRequest();
 xmlhttpi.onreadystatechange=function()
   {
@@ -52,7 +78,7 @@ xmlhttpi.onreadystatechange=function()
     }
   }
   
-  	var queryString = "?orderDate=" + encodeURIComponent(document.getElementById("orderDate").value) + 
+  	queryString = "?orderDate=" + encodeURIComponent(document.getElementById("orderDate").value) + 
 	"&unitPrice=" + encodeURIComponent(document.getElementById("unitPrice").value) +
 	"&orderComments=" + encodeURIComponent(document.getElementById("orderComments").value) +
 	"&creditDays=" + encodeURIComponent(document.getElementById("creditDays").value) +
@@ -67,21 +93,25 @@ xmlhttpi.open("GET","modules/orders/process_order.php"+queryString,true);
 xmlhttpi.send();
 
 
-	} // end save order
+} // end save order
 
 
-	$(function() {
-		$.datepicker.setDefaults( $.datepicker.regional[ "no" ] );
-		$( "#orderDate" ).datepicker( $.datepicker.regional[ "no" ] );	
-		});
+$(function() {
+	$.datepicker.setDefaults( $.datepicker.regional[ "no" ] );
+	$( "#orderDate" ).datepicker( $.datepicker.regional[ "no" ] );	
+	});
 
 
 
 function deleteOrder(companyName, orderStatusID, orderID) {
 
 
-jConfirm("<?php print $LANG['confirm_delete_order']; ?> "+companyName+" ?", "<?php print $LANG['confirm_delete_heading'];?>", function(y) {
-	if(y==true) {
+jConfirm("<?php print $LANG['confirm_delete_order']; ?> " + companyName + " ?", "<?php print $LANG['confirm_delete_heading'];?>", function(y) {
+	if (y==true) {
+
+		fetch(`modules/orders/delete_order.php?orderID=${orderID}`)
+		.then(res => statusBar.innerHTML = res);
+		// rewrite to fetch
 	xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function()  {
   		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
@@ -99,10 +129,12 @@ xmlhttp.send();
 
 
 function convertOrderStatus(orderID, orderStatusID) {
-
+	// avoid inline styles, rewrite to class
 	document.getElementById("allButton").style.visibility='visible';
 	
-	
+	fetch(`modules/orders/convert_order_status.php?orderID=${orderID}&orderStatusID=${orderStatusID}`)
+	.then(res => statusBar = res);
+
 xmlhttpi=new XMLHttpRequest();
 xmlhttpi.onreadystatechange=function()
   {
@@ -139,10 +171,10 @@ xmlhttpi.send();
 
 
 function showOrders(orderStatusID) {
-	if (orderStatusID!="all") {
+	if (orderStatusID != "all") {
 	getOrderName(orderStatusID)
 	} else {
-		document.getElementById("heading").innerHTML="<?php print $LANG['all_orders'];?>";
+		document.getElementById("heading").innerHTML = "<?php print $LANG['all_orders'];?>";
 
 		}
 
@@ -177,11 +209,16 @@ xmlhttp2.send();
 document.getElementById("data").innerHTML=""
 document.getElementById("iFrame").src=""
 document.getElementById("allButton").style.visibility='hidden';
-//document.getElementById(orderStatus).defaultSelected = true;
 }	
 	
 	
 function getOrderName(orderStatusID) {
+let heading = document.getElementById("heading");
+
+fetch(`modules/orders/get_order_name.php?orderStatusID=${orderStatusID}`)
+.then(res => heading.innerHTML = res);
+
+	// rewrite to fetch
 
 xmlhttpi=new XMLHttpRequest();
 xmlhttpi.onreadystatechange=function()
