3abf3a02d00cec6e94d6c55159050ea95287bb45
Ravi Brunsvik <ravi.brunsvik@gmail.com>
2020-04-21 23:14:40 +0200

    Fetch refactor.

--- modules/reports/js/reportFunctions.js
+++ modules/reports/js/reportFunctions.js
@@ -1,85 +1,42 @@
-let statusBar = document.getElementById("statusBar");
-let reportArea = document.getElementById("reportArea");
+import { get } from "../../../utils/fetchLib";
 
 function showRepSales(repID, year) {
+  // Show user Graphs
 
-	// Show user Graphs	
-	let graphArea = document.getElementById("repGraphArea");
-	// Rewrite styles to use a CSS class instead
-	graphArea.style.height = '1750px';
-	graphArea.style.width = '550px';
-	graphArea.style.visibility = 'visible';
-	
-	graphArea.src = "modules/reports/get_salesrep_charts.php?year=" + year + "&userID=" + repID;
-
-	// Show User Tables	
-	// rewrite to fetch
-	let rep = document.getElementById("repArea");
-	fetch(`modules/reports/rep_monthly_sales.php?userID=${repID}&year=${year}`)
-	.then(res => rep.innerHTML = res);
-
-	xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function () {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById("repArea").innerHTML = xmlhttp.responseText;
-		}
-	}
-
-	xmlhttp.open("GET", "modules/reports/rep_monthly_sales.php?userID=" + repID + "&year=" + year, true);
-	xmlhttp.send();
-
-} // end showSalesReport
-
+	// TODO: Rewrite styles to use a CSS class instead
+  let graphArea = document.getElementById("repGraphArea");
+  graphArea.style.height = "1750px";
+  graphArea.style.width = "550px";
+  graphArea.style.visibility = "visible";
+  graphArea.src =
+    "modules/reports/get_salesrep_charts.php?year=" + year + "&userID=" + repID;
+  // Show User Tables
+  document.getElementById("repArea").innerHTML = get(
+    `modules/reports/rep_monthly_sales.php?userID=${repID}&year=${year}`
+  );
+}
 
 function showTimeSheet(userID) {
-	// update to strict equality?
-	// where does displaySheet come from?
-	if (displaySheet == false) {
-		
-		// rewrite to fetch
-		fetch(`modules/reports/display_time_sheet.php?userID=${userID}`)
-		.then(res => reportArea.innerHTML = res);
-
-		xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function () {
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-				document.getElementById("reportArea").innerHTML = xmlhttp.responseText;
-			}
-		}
-		xmlhttp.open("GET", "modules/reports/display_time_sheet.php?userID=" + userID, true);
-		xmlhttp.send();
-		displaySheet = true;
-	} else {
-		reportArea.innerHTML = "";
-		statusBar.innerHTML = ""
-		displaySheet = false;
-	}
-
+  // where does displaySheet come from?
+  if (!displaySheet) {
+    document.getElementById("reportArea").innerHTML = get(
+      `modules/reports/display_time_sheet.php?userID=${userID}`
+    );
+    displaySheet = true;
+  } else {
+    reportArea.innerHTML = "";
+    statusBar.innerHTML = "";
+  }
 }
 
 function showSalesReport() {
-
-	statusBar.innerHTML = ""; // clean Status Bar
-
-	if (salesReport == false) {
-		// rewrite to fetch
-		fetch(`modules/reports/display_sales_report.php`)
-		.then(res => reportArea = res);
-		
-		xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function () {
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-				document.getElementById("reportArea").innerHTML = xmlhttp.responseText;
-			}
-		}
-		xmlhttp.open("GET", "modules/reports/display_sales_report.php", true);
-		xmlhttp.send();
-		salesReport = true;
-	} else {
-		reportArea.innerHTML = "";
-		// document.getElementById("reportArea").innerHTML = "";
-		salesReport = false;
-	}
-
+	document.getElementById("statusBar").innerHTML = "";
+  let reportArea = document.getElementById("reportArea");
+  if (!salesReport) {
+    // rewrite to fetch
+    reportArea.innerHTML = get(`modules/reports/display_sales_report.php`);
+    salesReport = true;
+  } else {
+    reportArea.innerHTML = "";
+  }
 }
-
