f1cce9e1ab6036a13cdaf80c62bb614d3b6cfc87
Terje Berg-Hansen <terje@itfakultetet.no>
2021-11-28 14:53:02 +0100

    database-tilgang med pg

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Custosales' });
+router.get('/', function (req, res, next) {
+
+  const { Client } = require('pg');
+  const client = new Client({
+    user: 'custosales',
+    host: 'custosales.com',
+    database: 'custosales',
+    password: 'custo432a',
+    port: 5432
+  });
+
+  client.connect();
+
+  client.query('SELECT * from users', (err, sql) => {
+
+    console.log(err, "Hei", sql.rows[0]);
+
+    res.render('index', {
+      title: 'Custosales',
+      sql: sql
+    });
+  
+    client.end();
+  });
+
+
+
+
 });
 
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   res.render('index', { title: 'Custosales' });
 });
 
