9fc5d097829dd6afd4668d29d367febec953599f
ravibrunsvik <ravi.brunsvik@gmail.com>
2021-12-05 23:25:25 +0100

    refactor: moved html to view. added pagination to query result.

--- public/javascripts/enheter.js
+++ public/javascripts/enheter.js
@@ -1,92 +1,61 @@
-window.onload = function onload() {
-  const div1 = document.querySelector("#div1");
-  const companyTable = document.querySelector("#div2");
+(function onload() {
+  const companyTable = document.querySelector("#tabell");
+  const form = document.querySelector('form');
+  const tableBody = document.querySelector("#tabell > tbody");
+  const searchField = document.querySelector('input[type=search]');
+  const previousPageButton = document.querySelector('#prev');
+  const nextPageButton = document.querySelector('#next');
 
-  const createCompanyTable = (enheter) => {
-    const table = document.createElement("table");
-    const tableHeader = document.createElement("tr");
-    const navn = document.createElement("th");
-    const orgnr = document.createElement("th");
 
-    table.id = "tabell";
-    table.className = "tabell";
+  const baseURL = "https://hotell.difi.no/api/json/brreg/enhetsregisteret";
+  let queryPage = 1;
 
-    navn.innerHTML = "Navn";
-    navn.className = "th";
-
-    orgnr.innerHTML = "Orgnummer";
-    orgnr.className = "th";
-
-    tableHeader.append(navn, orgnr);
-    table.append(tableHeader);
-
-    enheter.forEach((enhet) => {
+  const addResultRows = ({entries, page, pages}) => {
+    const rows = entries.map(({ navn, orgnr }) => {
       const row = document.createElement("tr");
       const navnCell = document.createElement("td");
       const orgnrCell = document.createElement("td");
 
-      navnCell.innerText = enhet.navn;
-      orgnrCell.innerText = enhet.orgnr;
+      navnCell.innerText = navn;
+      orgnrCell.innerText = orgnr;
 
       row.append(navnCell, orgnrCell);
-      table.append(row);
+      return row;
     });
-
-    companyTable.innerHTML = table.outerHTML;
+    return rows;
   };
 
-  const handleOnSend = async (event) => {
-    const brregURL =
-      "https://hotell.difi.no/api/json/brreg/enhetsregisteret?query=";
-    const query = `${brregURL}${searchField.value}`;
-
+  const handleOnSubmit = async (event) => {
+    companyTable.hidden = true;
+    tableBody.innerHTML = "";
+    event.preventDefault();
+    const url = `${baseURL}?query=${searchField.value}&page=${queryPage}`;
     try {
-      const request = await fetch(query);
-      const { entries: enheter } = await request.json();
-      createCompanyTable(enheter);
+      const request = await fetch(url);
+      const { entries, page, pages } = await request.json();
+      const resultRows = addResultRows({entries, page, pages});
+      resultRows.forEach(row => tableBody.append(row))
+      companyTable.hidden = false;
+      nextPageButton.disabled = page === pages;
+      previousPageButton.disabled = page < pages;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const createSearchSection = () => {
-    const handleOnSearch = (event) => {
-      if (event.key !== "Enter") return;
-      event.preventDefault();
-
-      if (!searchField.value) {
-        alert("Tast inn firmanavn eller orgnummer ");
-        searchField.focus();
-        return;
-      }
-      sendBtn.click();
-    };
-
-    const sendButton = document.createElement("button");
-    const searchField = document.createElement("input");
-    const label = document.createElement("label");
-
-    sendButton.className = "btn btn-success";
-    sendButton.innerText = "Søk";
-    sendButton.addEventListener("click", handleOnSend);
-
-    searchField.type = "search";
-    searchField.id = "sokefelt";
-    searchField.placeholder = "Søk etter navn eller orgnummer";
-    searchField.addEventListener("keyup", handleOnSearch);
-
-    label.innerText = "Søk i enhetsregisteret:";
-    label.htmlFor = "sokefelt";
-
-    div1.append(label, searchField, sendButton);
+  const nextPage = (event) => {
+    queryPage += 1;
+    handleOnSubmit(event);
   };
 
-  div1.innerHTML = "";
-  companyTable.innerHTML = "";
+  const previousPage = (event) => {
+    queryPage -= 1;
+    handleOnSubmit(event);
+  }
 
-  createSearchSection();
+  nextPageButton.onclick = nextPage;
+  previousPageButton.onclick = previousPage;
 
-  const searchField = document.querySelector("#sokefelt");
-
-  searchField.focus();
-};
+  form.onsubmit = handleOnSubmit;
+  
+})();
