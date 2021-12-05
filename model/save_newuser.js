c2bd9e60bc047ac8489f86e568b621a85fa8946d
Terje Berg-Hansen <terje@itfakultetet.no>
2021-12-05 02:42:05 +0100

    save new user

--- model/save_newuser.js
+++ model/save_newuser.js
@@ -1,7 +1,9 @@
 
 ;(async () => {
   const db = require('./db');
-   
+  const bcrypt = require('bcrypt');
+  const saltRounds = 10;
+     
   // Connect
     let client = await db.pool.connect()
     console.log("Database connected from model/save_newuser");
@@ -13,8 +15,15 @@ let save = (req) => {
   const password_plain = req.body.password;
   const password = bcrypt.hashSync(password_plain, saltRounds);
   const user_email = req.body.user_email;
-  const start_date = req.body.start_date;
-  const end_date = req.body.end_date;
+  let start_date = null;
+  if(req.body.start_date!='') {
+    start_date = req.body.start_date;
+  }
+  let end_date = null;
+ if(req.body.end_date!='') {
+   end_date = req.body.end_date;
+  } 
+    
   const title_id = req.body.title_id;
   const address = req.body.address;
   const zip = req.body.zip;
@@ -25,18 +34,17 @@ let save = (req) => {
   const supervisor_id = req.body.supervisor_id;
   const workplace_id = req.body.workplace_id;
   const department_id = req.body.department_id;
-  const manager_id = req.body.manager_id;
   const user_comments = req.body.user_comments;
  
   const sql = {
     text: 'INSERT INTO users(first_name,last_name,username,password,user_email,'
-    +'start_date,end_date,title_id,address,zip,city,signed_contract,enabled,skills,supervisor_id,workplace_id,manager_id.user_comments)'+
-    +' VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17 ,$18, $19)',
-    values: [first_name,last_name,username,password,user_email,start_date,end_date, title_id,address,zip,city,signed_contract,enabled,skills,supervisor_id,workplace_id,department_id,manager_id,user_comments],
+    +'start_date,end_date,title_id,address,zipcode,city,signed_contract,enabled,skills,supervisor_id,workplace_id,department_id,user_comments)'
+    +' VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)', values: [first_name,last_name,username,password,user_email,start_date,end_date,title_id,address,zip,city,signed_contract,enabled,skills,supervisor_id,workplace_id,department_id,user_comments]
   }
   
   // Query
   try {
+    console.log(sql);
     const newuser = client.query(sql);
 } finally {
   // Make sure to release the client before any error handling,
