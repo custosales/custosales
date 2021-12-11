b9405a5c44a0adbc8b45a908968a9bf6f082e2a9
Terje Berg-Hansen <terje@itfakultetet.no>
2021-12-11 16:08:59 +0100

    new project

--- routes/modules/admin/newproject.js
+++ routes/modules/admin/newproject.js
@@ -1,16 +1,45 @@
 var express = require("express");
+const { getTitles } = require("../../../model/get_titles");
+const { getDepartments } = require("../../../model/get_departments");
+const { getWorkPlaces } = require("../../../model/get_workplaces");
+const { getManagers } = require("../../../model/get_managers");
+const { saveNewUser } = require("../../../model/save_newuser");
 var router = express.Router();
-const { getProjects } = require("../../../model/admin/get_projects");
 
-// GET users page.
+/* GET newuser page. */
 router.get("/", async function (req, res, next) {
-  const { rows } = await getProjects();
-  const response = {
-    title: "Prosjektoversikt",
-    rows,
-  };
-  
-  res.render("modules/admin/projects", response);
+  const get_titles = getTitles();
+  const get_managers = getManagers();
+  const get_departments = getDepartments();
+  const get_workplaces = getWorkPlaces();
+
+  const [titles, managers, departments, workplaces] = await Promise.all([
+    get_titles,
+    get_managers,
+    get_departments,
+    get_workplaces,
+  ]);
+
+  res.render("/modules/admin/newproject", {
+    title: "Nytt prosjekt",
+    message: "",
+    titles: titles.rows,
+    departments: departments.rows,
+    workplaces: workplaces.rows,
+    managers: managers.rows,
+  });
 });
+
+/* Register new user. */
+router.post("/", async function (req, res, next) {
+  const saved = await saveNewUser(req);
+  console.log(saved);
+  res.redirect("/modules/admin/projects");
+  // res.render('modules/admin/newuser', {
+  //     title: 'Ny bruker',
+  //     message: `Ny bruker lagret`
+
+  // });
+}); // end router.post
 
 module.exports = router;
